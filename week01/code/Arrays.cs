54aa5b331c7661a1f0408022f388acebd3cef9c0
jackloosantamaria <jackloosantamaria@hotmail.com>
2025-05-05 11:31:01 -0500

    W01 Code: Dynamic Arrays Assignment

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,16 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //create array length size
+        double [] multiples = new double[length];
+
+        //loop to fill array
+        for (int i=0; i < length; i++){
+            //calculate number
+            multiples[i] = number * (i+1);
+        }
+
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +38,16 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //Get the last elements to place in front
+        List<int> tail = data.GetRange(data.Count - amount, amount);
+
+        //Get rid of the last elements to avoid duplicates
+        data.RemoveRange(data.Count - amount, amount);
+
+        //Insert elements at the beginning of the list
+        data.InsertRange(0, tail);
+
+        
     }
 }
