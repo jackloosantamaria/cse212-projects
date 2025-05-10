78b97e098ee2d135bf18aca17f2db18f18fd4520
jackloosantamaria <jackloosantamaria@hotmail.com>
2025-05-10 09:05:40 -0500

    working on team activity W02 step 4

--- week02/teach/Program.cs
+++ week02/teach/Program.cs
@@ -1,3 +1,5 @@
 ﻿Console.WriteLine("\n======================\nCustomer Service\n======================");
 CustomerService.Run();
+Console.WriteLine("\n======================\nComplex Stack\n======================");
+ComplexStack.Main();
 // CustomerServiceSolution.Run();
