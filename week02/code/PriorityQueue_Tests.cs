4f63a12ec51bebc0b754c93e70828132a933bd40
jackloosantamaria <jackloosantamaria@hotmail.com>
2025-05-15 19:07:50 -0500

    added a few final comments

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,9 +6,9 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Enqueue three items with different priorities: A(2), B(5), C(3). Then dequeue once.
+    // Expected Result: B should be returned, as it has the highest priority (5).
+    // Defect(s) Found: the for-loop excludes the last element, causing it to skip comparisons with the last item in the queue.
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
@@ -23,9 +23,9 @@ public class PriorityQueueTests
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Enqueue three items with the same priority: A(4), B(4), and C(2). Dequeue twice.
+    // Expected Result: A and B should be dequeued FIFO since they have the same priority.
+    // Defect(s) Found: the for-loop excludes the last item, which could break the FIFO if a highsted priority item at the end.
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
